509221d5f9448c98839ae999c5a1c8aec958ab14
Jeferson Santos <jefo2k@gmail.com>
2020-08-04 23:52:46 -0300

    add ant design and test button element

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
+import Button from 'ant-design-vue/lib/button';
+import 'ant-design-vue/dist/antd.css';
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+Vue.component(Button.name, Button);
 
 new Vue({
   render: h => h(App),
